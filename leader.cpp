a39d0bc40c1444725690b087e45bba12da826f10
amit kumar <amitkumaramit118589@gmail.com>
2025-10-13 19:27:50 +0530

    Create leader.cpp

---
+++ leader.cpp
@@ -0,0 +1,20 @@
+vector<int> findLeaders(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> leaders;
+    int maxRight = arr[n - 1];
+
+    
+    leaders.push_back(maxRight);
+
+    
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] > maxRight) {
+            maxRight = arr[i];
+            leaders.push_back(maxRight);
+        }
+    }
+
+    
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+}
