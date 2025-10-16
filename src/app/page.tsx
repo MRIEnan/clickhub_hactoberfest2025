645a21a788b06443ef030c0e40fb4c17d4544a20
Kumari Vaishnavi <145796948+vaishnavijha12@users.noreply.github.com>
2025-10-16 11:53:09 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import SmoothAuroraButton from '../SmoothAuroraButton';
-
 import ButtonGallery from './components/ButtonGallery';
 import { loadButtonContributions } from '@/utils/buttonLoader';
 
@@ -9,6 +8,14 @@ export default async function Home() {
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
+      {/* Aurora Button Section */}
+      <div className="flex items-center justify-center my-12">
+        <SmoothAuroraButton onClick={() => alert('Aurora Button Clicked!')}>
+          Click Me
+        </SmoothAuroraButton>
+      </div>
+
+      {/* Button Gallery Section */}
       <ButtonGallery contributions={contributions} />
     </div>
   );
