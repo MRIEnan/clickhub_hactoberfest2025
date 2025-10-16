c5b8a988c84f4c5d9c8f4bc700cfacfeb544acc3
Kumari Vaishnavi <145796948+vaishnavijha12@users.noreply.github.com>
2025-10-16 11:51:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
+import '../src/globals.css';
+
+
 import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
 // Import global CSS styles
 import "./globals.css";
